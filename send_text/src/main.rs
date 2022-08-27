42a1654e0ea47c1f678773d6f4eccf204e83dd87
Annika Hannig <annika@hannig.cc>
2022-08-27 12:15:43 +0000

    updated example

--- send_text/src/main.rs
+++ send_text/src/main.rs
@@ -1,32 +1,23 @@
-
 use anyhow::Result;
 
-use airportdisplay::{
-    Display,
-    Command,
-    Text,
-    TextBuffer,
-};
+use airportdisplay::{text::Buffer, Command, Display};
 
 use std::io;
 
+/// Send text read from stdio to the display
 fn main() -> Result<()> {
-
-    // Read buffer
-    let mut buffer = String::new();
+    // Read stdin into buffer
+    let mut text = String::new();
     for line in io::stdin().lines() {
-        if let Ok(line) = line {
-            buffer.push_str(line.as_str()); 
-            buffer.push('\n');
-        }
+        text.push_str(line?.as_str());
+        text.push('\n');
     }
 
+    // Send content to display
     let display = Display::open("172.23.42.29:2342".into())?;
-    let cmd = Command::Text(Text::Buffer(TextBuffer::from(buffer)));
-    
+
     display.send(Command::Clear)?;
-    display.send(cmd)?;
+    display.send(Buffer::from(text).into())?;
 
     Ok(())
 }
-
