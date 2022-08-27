005bc6d2973ec01d451ff2c38787547cebcf99ee
Annika Hannig <annika@hannig.cc>
2022-08-27 12:02:42 +0000

    updated hello display example

--- hello_display/src/main.rs
+++ hello_display/src/main.rs
@@ -2,8 +2,8 @@ use anyhow::Result;
 use airportdisplay::{
     Command,
     Display,
-    Text,
-    TextBuffer,
+    text::Text,
+    text::Buffer,
 };
 
 fn main() -> Result<()> {
@@ -11,8 +11,8 @@ fn main() -> Result<()> {
     let display = Display::open("172.23.42.29:2342".into())?;
 
     let text = Command::Text(Text::Buffer(
-        TextBuffer::from(
-            "♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥\n♥ mauuu          ♥\n♥ mau mauuuunz!  ♥\n♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥".into())));
+        Buffer::from(
+            "♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥\n♥ mauuu          ♥\n♥ mau mauuuunz!  ♥\n♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥".to_string())));
 
     display.send(Command::Clear)?;
     display.send(text)?;
