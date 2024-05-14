5803b71f3af743270516483f1d2d715d3b100f15
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-05-14 21:41:59 +0200

    fix c example

--- examples/lang_c/main.c
+++ examples/lang_c/main.c
@@ -8,7 +8,7 @@ int main(void) {
 
     sp2_PixelGrid *pixels = sp2_pixel_grid_new(sp2_PIXEL_WIDTH, sp2_PIXEL_HEIGHT);
     sp2_pixel_grid_fill(pixels, true);
-    command = sp2_command_bitmap_linear_win(0, 0, pixels);
+    sp2_Command *command = sp2_command_bitmap_linear_win(0, 0, pixels);
     if (command == NULL)
         return 4;
     if (!sp2_connection_send(connection, command))
