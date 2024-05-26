edcad6fd03cd923d6cab624a7283ea4a76d19721
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-05-26 11:40:52 +0200

    improve c build

--- examples/lang_c/build.rs
+++ examples/lang_c/build.rs
@@ -1,8 +1,15 @@
-
 fn main() {
     println!("cargo::rerun-if-changed=src/main.c");
+    println!("cargo::rerun-if-changed=build.rs");
 
-    cc::Build::new()
-        .file("src/main.c")
-        .compile("lang_c");
+    let sp_include = std::env::var_os("DEP_SERVICEPOINT_INCLUDE")
+        .unwrap()
+        .into_string()
+        .unwrap();
+
+    // this builds a lib, this is only to check that the example compiles
+    let mut cc = cc::Build::new();
+    cc.file("src/main.c");
+    cc.include(&sp_include);
+    cc.compile("lang_c");
 }
