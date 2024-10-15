a858261540a06a5445bcdb1ab02d19314ac2f691
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-10-15 22:54:32 +0200

    use constants from generated file

--- crates/servicepoint_binding_cs/ServicePoint/Constants.cs
+++ crates/servicepoint_binding_cs/ServicePoint/Constants.cs
@@ -1,15 +1,17 @@
+using ServicePoint.BindGen;
+
 namespace ServicePoint;
 
 public static class Constants
 {
     /// size of a single tile in one dimension
-    public const int TileSize = 8;
+    public const int TileSize = NativeMethods.SP_TILE_SIZE;
 
     /// tile count in the x-direction
-    public const int TileWidth = 56;
+    public const int TileWidth = NativeMethods.SP_TILE_WIDTH;
 
     /// tile count in the y-direction
-    public const int TileHeight = 20;
+    public const int TileHeight = NativeMethods.SP_TILE_SIZE;
 
     /// screen width in pixels
     public const int PixelWidth = TileWidth * TileSize;
